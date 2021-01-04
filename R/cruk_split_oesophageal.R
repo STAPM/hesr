487c45e70dbac0e11bcbad7c67c17881ee42b89e
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2021-01-04 15:32:36 +0000

    edits in course of tob workflow review

--- R/cruk_split_oesophageal.R
+++ R/cruk_split_oesophageal.R
@@ -1,15 +1,19 @@
 
-#' CRUK Oesophageal split \lifecycle{maturing}
+#' CRUK Oesophageal split \lifecycle{stable}
 #'
-#' This code splits the diagnosis codes for Oesophageal cancer (C15) in the HES data in to SCC or AC using CRUK splits, which are formatted and kept
-#' in the data-raw folder `prep_cruk_splits`. Make sure there are no NAs in the startage field before running this. I.e. clean HES data first. This function is used
-#' within the function `narrow_method()`.
+#' This code splits the diagnosis codes for Oesophageal cancer (C15) 
+#' in the HES data in to SCC or AC using CRUK splits, which are formatted and kept
+#' in the data-raw folder `prep_cruk_splits`. 
+#' Make sure there are no NAs in the startage field before running this. I.e. clean HES data first. 
 #'
+#' @param hes Data.table - the cleaned HES data.
+#' @param splits Data.table - the distribution of C15 cases by SCC and AC
+#' @param var_name Character string - the name of the variable containing the C15 ICD-10 code to be split 
 #'
+#' @importFrom data.table := rbindlist
 #'
-#' @param hes is the cleaned HES data.
-#'
-#' @return Returns an updated version of the HES data, with the diagnoses codes for oesophageal cancer (C15) split proportionally between Oesophageal SCC and Oesophageal AC.
+#' @return Returns an updated version of the HES data.table, with the diagnoses codes for 
+#' oesophageal cancer (C15) split proportionally between Oesophageal SCC and Oesophageal AC.
 #'
 #' @export
 #'
@@ -22,9 +26,17 @@
 #' }
 #'
 cruk_split_oesophageal <- function(
-  hes
+  hes,
+  splits = hesr::cruk_splits,
+  var_name = "icd_code"
 ) {
-
+  
+  # Check startage variable for NAs
+  testthat::expect_identical(
+    nrow(hes[is.na(startage)]), 0,
+    info = "There are NAs in the startage variable"
+  )
+  
   # create age bands to match those used by cruk
   hes[, cruk_ageband := c(
     "00-04",
@@ -45,28 +57,36 @@ cruk_split_oesophageal <- function(
     "75-79",
     "80-84",
     "85+")[findInterval(startage, c(0, seq(5, 85, 5)))]]
-
-  # separate out oesophagus morb
-
-  oesophagus_morb <- hes[icd_code == "C15"]
-
-  # merge with cruk splits
-  oesophagus_morb <- merge(oesophagus_morb, hesr::cruk_splits, by = c("cruk_ageband", "sex"), all.x = T, all.y = F)
-
-  oesophagus_morb[ , icd_code := as.character(sapply(proportion_scc, function(x) sample(c("C15s", "C15a"), size = 1, prob = c(x, 1-x))))]
-
+  
+  # Separate out oesophagus morb
+  
+  oesophagus_morb <- hes[get(var_name) == "C15"]
+  
+  # Merge with cruk splits
+  oesophagus_morb <- merge(oesophagus_morb, splits, by = c("cruk_ageband", "sex"), all.x = T, all.y = F)
+  
+  # Make the splits
+  oesophagus_morb[ , (var_name) := as.character(
+    sapply(proportion_scc, 
+           
+           function(x) {
+             sample(c("C15_SCC", "C15_AC"), size = 1, prob = c(x, 1-x))
+           }
+           
+    ))]
+  
   oesophagus_morb[ , proportion_scc := NULL]
-
+  
   # strip C15 out of hes data
-  hes <- hes[icd_code != "C15"]
-
-  # join on the split oesophagus morb to include C15s and C15a
+  hes <- hes[get(var_name) != "C15"]
+  
+  # Join on the split oesophagus morb to include C15s and C15a
   hes <- rbindlist(list(hes, oesophagus_morb), use.names = T)
   hes[ , cruk_ageband := NULL]
-
+  
   # Test
   # hes[icd_code == "C15a"]
-
-  return(hes)
-
+  
+  
+  return(hes[])
 }
