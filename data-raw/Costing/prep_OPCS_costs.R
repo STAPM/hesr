0f05665f377a6c22bf9f2274da6a19f4c43c7c30
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2021-04-21 08:01:47 +0100

    move unit cost code and data from package to repo

--- data-raw/Costing/prep_OPCS_costs.R
+++
@@ -1,7 +0,0 @@
-# Read in OPCS lookup table
-
-opcs_costs <- fread("//tsclient/X/ScHARR/PR_HES_data_TA/Code/Costing/OPCS/OPCS_2016.csv")[ , c("type", "cost", "OPCS"), with = F]
-
-
-# Embed the data within the package
-usethis::use_data(opcs_costs, overwrite = TRUE)
